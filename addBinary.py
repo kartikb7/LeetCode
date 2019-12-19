e44fd4051f3c6c7f0d282e468a0a7303e07af204
Kartik Bansal <kartikbansal007@gmail.com>
2019-12-19 12:34:52 -0500

    Runs for large Binary numbers too

--- addBinary.py
+++ addBinary.py
@@ -3,23 +3,20 @@ class Solution:
 
         binDec = 0
         for i in range(len(a)):
-            binDec += int(a[::-1][i]) * math.pow(2, i)
+            binDec += int(a[::-1][i]) * int(math.pow(2, i))
 
         binDec2 = 0
         for i in range(len(b)):
-            binDec2 += int(b[::-1][i]) * math.pow(2, i)
+            binDec2 += int(b[::-1][i]) * int(math.pow(2, i))
 
         binSum = binDec + binDec2
-        print(binDec)
-        print(binDec2)
+
         result = ""
         if binSum == 0:
             result = "0"
 
         while (binSum > 0):
-            result += str(int(binSum % 2))
-            binSum = (binSum - binSum % 2) / 2
+            result += str(binSum % 2)
+            binSum = binSum//2
 
         return result[::-1]
-
-# Doesn't give the correct answer for large binary numbers
