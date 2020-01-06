14bcc7f98015e278b1db0666d9a2a8d9c2ecf79b
Kartik Bansal <kartikbansal007@gmail.com>
2020-01-06 18:58:23 -0500

    Solution using Factorial

--- ClimbingStairs.py
+++ ClimbingStairs.py
@@ -1,2 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        if n == 0:
+            return 0
+
+        if n % 2 == 0:
+            maxTwoCoins = n / 2
+        else:
+            maxTwoCoins = (n - 1) / 2
+
+        solution = 0
+        for i in range(int(maxTwoCoins) + 1):
+            solution = solution + math.factorial(n - i) / (math.factorial(n - 2 * i) * math.factorial(i))
+
+        return int(solution)
