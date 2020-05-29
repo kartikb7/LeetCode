c5854961c44342169c69a902a9a92e3d69b88c26
Kartik Bansal <kartikbansal007@gmail.com>
2020-05-29 01:04:24 -0400

    WIP for valid parentheses

---
+++ 20. Valid Parentheses.py
@@ -0,0 +1,40 @@
+def search_closing(string, i, close):
+    found = False
+    while not found:
+        if string[i] == close:
+            return True
+        if string[i] == '(':
+            if search_closing(string, i + 1, ')'):
+                continue
+            else:
+                return False
+        if string[i] == '{':
+            if search_closing(string, i + 1, '}'):
+                continue
+            else:
+                return False
+        if string[i] == '[':
+            if search_closing(string, i + 1, ']'):
+                continue
+            else:
+                return False
+
+
+class Solution(object):
+
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        valid = True
+
+        if len(s) != 0:
+            if s[0] == '(':
+                valid = search_closing(s, 1, ')')
+            if s[0] == '{':
+                valid = search_closing(s, 1, '}')
+            if s[0] == '[':
+                valid = search_closing(s, 1, ']')
+
+        return valid
